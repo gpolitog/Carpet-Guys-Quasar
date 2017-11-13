67b5365a650e89e2ff0835e74bc5f9128ee4104e
Spiralis-Solutions <krisstjohn96@gmail.com>
2017-11-13 10:59:28 -0700

    cosmetics of nav and div of customer screen??

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="main">
-    account
+    <div class="status"></div>
+    <div class="usercontact"></div>
+    <div class="jobcontact"></div>
+    <div class="promo"></div>
   </div>
 </template>
 
@@ -22,6 +25,31 @@
 
 <style scoped lang="less">
   .main {
-  }
-
+    margin-top: 10px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 100px 200px 100px;
+    overflow: hidden;
+    }
+    .status {
+      grid-column-start: 1;
+      grid-column-end: 3;
+      grid-row: 1;
+    }
+    .usercontact {
+      grid-column-start: 1;
+      grid-column-end: 1;
+      grid-row: 2;
+    }
+    .jobcontact {
+      grid-column-start: 2;
+      grid-column-end: 2;
+      grid-row: 2;
+    }
+    .promo {
+      grid-column-start: 1;
+      grid-column-end: 3;
+      grid-row: 3;
+    }
 </style>
