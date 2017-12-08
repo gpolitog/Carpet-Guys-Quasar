0fca2fd7020b9d06fc4cc64594048ae940cd1395
Grapetoast <bonham.goodman@gmail.com>
2017-12-07 17:43:37 -0700

    .

---
+++ src/components/elements/mapbox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+    <h1>mapbox</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Mapbox'
+  }
+</script>
+<style scoped lang="less">
+  .main {
+    margin-top: 160px;
+  }
+</style>
